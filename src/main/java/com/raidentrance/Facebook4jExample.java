a09a9d0b8e30a89efd27dc5b17a2a70571e4da68
Alex <raidentrance@gmail.com>
2018-03-15 08:53:56 -0600

    initial example

---
+++ src/main/java/com/raidentrance/Facebook4jExample.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.raidentrance;
+
+import facebook4j.Facebook;
+import facebook4j.FacebookException;
+import facebook4j.FacebookFactory;
+import facebook4j.Friend;
+import facebook4j.ResponseList;
+import facebook4j.auth.AccessToken;
+
+/**
+ * @author raidentrance
+ *
+ */
+public class Facebook4jExample {
+	public static void main(String[] args) throws FacebookException {
+		Facebook facebook = new FacebookFactory().getInstance();
+		facebook.setOAuthAppId("", "");
+		facebook.setOAuthAccessToken(new AccessToken(""));
+		ResponseList<Friend> friends = facebook.getFriends();
+		for (Friend friend : friends) {
+			System.out.println(friend);
+		}
+	}
+}
